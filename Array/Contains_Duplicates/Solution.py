6d544c7ab714c1fc679e1453f21f0ee8b9e9efdf
Pratyaksh <pratyaksh2000saini@gmail.com>
2020-10-01 15:31:51 +0530

    Contains Duplicates added

---
+++ Array/Contains_Duplicates/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return False
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
