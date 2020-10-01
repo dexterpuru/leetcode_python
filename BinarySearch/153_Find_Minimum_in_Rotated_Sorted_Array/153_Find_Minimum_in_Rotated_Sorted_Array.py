b42a87ad1440dc06550384c5891300fa0ad43711
Gagan Verma <46402550+zatch3301@users.noreply.github.com>
2020-10-01 19:41:04 +0530

    Added BinarySearch and String leetcode solution

---
+++ BinarySearch/153_Find_Minimum_in_Rotated_Sorted_Array/153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMin(self, nums):
+        # A[l] > A[r]
+        l, r = 0, len(nums) - 1
+        while l < r and nums[l] >= nums[r]:
+            mid = (l + r) / 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
